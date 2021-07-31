ab4e54e5f49d866641379850e4abdc0b73761d8d
samyakgaur <samyakgaur@yahoo.com>
2021-07-31 17:40:27 +0530

    beat testing 2

--- users.go
+++ users.go
@@ -296,7 +296,7 @@ func GetUsersOptionPresence(n bool) GetUsersOption {
 func newUserPagination(c *Client, options ...GetUsersOption) (up UserPagination) {
 	up = UserPagination{
 		c:     c,
-		limit: 1, // per slack api documentation.
+		limit: 200, // per slack api documentation.
 	}
 
 	for _, opt := range options {
